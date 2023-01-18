1162dd0bfdd5e9d987da3484d2a5be31ae5a5e3d
Armend Veseli <1509793+Vodkacannon@users.noreply.github.com>
2023-01-18 09:44:59 -0500

    Update number_guesser.py

--- src/number_guesser.py
+++ src/number_guesser.py
@@ -1,6 +1,9 @@
+current_guess = 0
 current_steps = 0
+is_correct_guess = False
 target_steps = 10
 target_number = 0
+
 
 def get_number_guess():
     return input("Enter a number to guess: ")
@@ -33,3 +36,43 @@ def print_number_steps():
 
 def print_number_guess_correct():
     print("You got it right in " + str(current_steps) + ".")
+
+
+def is_guess_lower(current_guess):
+    if current_guess < target_number:
+        return True
+    else:
+        return False
+    
+
+def is_guess_higher(current_guess):
+        if current_guess > target_number:
+        return True
+    else:
+        return False
+
+
+def print_guess_is_lower():
+    return print("Your guess is low.")
+
+
+def print_guess_is_higher():
+    return print("Your guess is high.")
+
+    
+def main_game_loop():
+    while not is_correct_guess:
+        global current_guess
+        current_guess = get_number_guess()
+        increment_steps()
+        
+        if is_number_guess_correct(current_guess):
+            print_number_guess_correct()
+        else:
+            print_number_steps()
+            
+        if is_guess_lower():
+            print_guess_is_low()
+        elif is_guess_higher():
+            print_guess_is_high()
+            
